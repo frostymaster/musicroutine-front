6cb95b2d545645cf74bd690746c563f15d9b3bf6
frostymaster <frostmasterwork@gmail.com>
2019-01-07 01:19:14 +0300

    Make request to api

--- musicroutine/src/pages/SignUpForm.js
+++ musicroutine/src/pages/SignUpForm.js
@@ -1,23 +1,70 @@
 import React, {Component} from 'react';
 
 class SignUpForm extends Component{
+
+    constructor() {
+      super();
+
+      this.state = {
+          email: '',
+          password: '',
+          name: ''
+      }
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(e) {
+    let target = e.target;
+    let value = target.type === "checkbox" ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+        [name] : value
+    });
+
+}
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("WOW SUBMITED");
+      fetch('http://localhost:5000/api/v1/user', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin' : '*'
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        password: this.statepassword,
+        email: this.state.email
+      })
+    });
+}
+
     render(){
         return (
             <div className="FormCenter">
               <form className="FormFields" onSubmit={this.handleSubmit}>
                 <div className="FormField">
                   <label className="FormField__Label" htmlFor="name">Full Name</label>
-                  <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
+                  <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" 
+                  name="name" value={this.state.name} onChange={this.handleChange}/>
                 </div>
 
                 <div className="FormField">
                   <label className="FormField__Label" htmlFor="password">Password</label>
-                  <input type="text" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
+                  <input type="text" id="password" className="FormField__Input" placeholder="Enter your password" 
+                  name="password" value={this.state.password} onChange={this.handleChange}/>
                 </div>
 
                 <div className="FormField">
                   <label className="FormField__Label" htmlFor="email">E-mail</label>
-                  <input type="text" id="email" className="FormField__Input" placeholder="Enter your e-mail" name="email"/>
+                  <input type="text" id="email" className="FormField__Input" placeholder="Enter your e-mail" 
+                  name="email" value={this.state.email} onChange={this.handleChange}/>
                 </div>
 
                 <div className="FormField">
